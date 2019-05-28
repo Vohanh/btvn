ddac7e3fc388e14e046c947883113967e9dbedba
Võ Hạnh <vohanh48@gmail.com>
2019-05-29 05:20:52 +0700

    create nsx

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <footer className="App-footer">
+        <p>Day la footer</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
