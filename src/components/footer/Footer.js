5c4d2cbe84fcc9ee57174cbe96deb9f165f640da
Võ Hạnh <vohanh48@gmail.com>
2019-05-29 05:49:40 +0700

    css-footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import '../../assets/Main.css'
 function App() {
   return (
     <div className="App">
-      <footer className="App-footer">
+      <footer className="App-Footer">
         <p>Footer</p>
       </footer>
     </div>
