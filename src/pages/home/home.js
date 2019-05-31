1f972ad72507ec4b98a564e3a26dc40476aaa12a
Vohanh <duchanh.7295@gmail.com>
2019-05-31 20:58:10 +0700

    fix1

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import '../../assets/css/main.css'
 class HomePage extends React.Component {
-    render () {
+    constructor() {
+        super()
+        this.state = {
+            data: [
+                { name: "Hanh" },
+                { name: "Mai" },
+                { name: "Nguyen" },
+                { name: "Quyen" },
+                { name: "Son" },
+                { name: "Dung" },
+                { name: "An" },
+                { name: "Hoang" },
+                { name: "Thuan" },
+                { name: "Vinh" },
+                { name: "Do" },
+            ]
+        }
+        console.log(this.state.data)
+    }
+    render() {
         return (
             <React.Fragment>
                 <div className="home-page-top">
-                    Hello
-                </div>
-                <div>
-                    HomePage
+                    <ul>
+                        {
+                            this.state.data.map((abcd, index) =>
+                                <li>
+                                    <span className="abc">Name:</span>
+                                    <span className="abc1">{abcd.name}</span>
+                                </li>
+                            )}
+                    </ul>
                 </div>
             </React.Fragment>
         )
